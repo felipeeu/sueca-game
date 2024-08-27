9519194dd97c37a84e8f0983659726ec3ac09f35
Felipe Domingues <felipeeu@yahoo.com.br>
2024-08-27 17:30:22 -0300

    refactor view in order to use css in cards

--- src/sueca_game/views.cljs
+++ src/sueca_game/views.cljs
@@ -2,8 +2,13 @@
   (:require
    [re-frame.core :as re-frame]
    [sueca-game.subs :as subs]
-   [sueca-game.events :as events]))
+   [sueca-game.events :as events]
+   [clojure.string :refer [lower-case]]))
 
+(def css-suits {:♦ "diams"
+                :♠ "spades"
+                :♣ "clubs"
+                :♥ "hearts"})
 (defn render-deck
   [table round round-end?]
   (let [cards-by-round ((keyword (str round)) table)]
@@ -12,32 +17,53 @@
 
      [:button {:on-click (fn [] (re-frame/dispatch [::events/increment-round])) :disabled (not round-end?)} "get cards"]]))
 
-(defn render-cards [player turn round-end?]
 
-  (map (fn [value]
-         (let [id (:id player)
-               disabled (or (not (= id (str turn)))  round-end?)]
-           [:div
-            [:button {:on-click (fn []
-                                  (re-frame/dispatch [::events/select-card value]))
-                      :disabled  disabled}
-             (second value)
-             (first value)]]))
-       (:hand player)))
+(defn card-creator
+  [values & args]
+  (let [round-end? @(re-frame/subscribe [::subs/round-end?])
+        turn @(re-frame/subscribe [::subs/turn])
+        suit ((keyword (first values)) css-suits)
+        number (lower-case (second values))
+        player (second args)
+        id (:id player)
+        has-event (not (nil? (first args)))
+        is-player-turn? (= id (str turn))
+        is-dispatchable?  (and is-player-turn?
+                               has-event
+                               (not round-end?))]
+
+    [:li {:key (str id suit number)}
+     [:a {:class (str "card " "rank-" number " " suit)
+          :on-click (if is-dispatchable?
+                      (fn []  (re-frame/dispatch [(first args) values]))
+                      nil)
+          :style {:cursor "pointer"}}
+      [:span {:class "rank"} (second values)]
+      [:span {:class "suit"} (first values)]]]))
+
+(defn render-table
+  [table round]
+  (let [table-by-round ((keyword (str "round" round)) table)]
+    [:div {:class "playingCards"}
+     [:ul {:class "table"}
+      (map #(card-creator (second %)) table-by-round)]]))
 
 
-(defn add-player [player turn round-end?]
+(defn render-hand [player]
+  [:ul {:class "hand"}
+   (map #(card-creator % ::events/select-card player) (:hand player))])
+
+(defn add-player [player]
   [:div {:key (:id player)}
    [:p (str "player " (:id player)) ": " (:name player)]
-   [:span "hand:" (render-cards player turn round-end?)]])
+   [:div {:class "playingCards rotateHand"} (render-hand player)]])
 
-(defn render-players [player-list turn round-end?]
-  (map #(add-player % turn round-end?) player-list))
+(defn render-players [player-list]
+  (map #(add-player %) player-list))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])
         players-cards @(re-frame/subscribe [::subs/players])
-        turn @(re-frame/subscribe [::subs/turn])
         round @(re-frame/subscribe [::subs/round])
         table @(re-frame/subscribe [::subs/table])
         started? @(re-frame/subscribe [::subs/started?])
@@ -45,8 +71,9 @@
     [:div
      [:h1
       "Hello from " @name]
-     [:button  {:on-click (fn [] (re-frame/dispatch [::events/start-game 1])) :disabled started?} "start game"]
+     [:button  {:on-click (fn [] (re-frame/dispatch [::events/start-game 1]))
+                :disabled started?} "start game"]
 
-     [:div "table: " table]
      (render-deck table round round-end?)
-     (render-players players-cards turn round-end?)]))
+     (render-players players-cards)
+     (render-table table round)]))
